25b0f644923a2eb0949f565cd884a74373076758
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2022-12-14 19:31:40 +0100

    feat: Add basic command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+// Copyright (c) 2022 Jan Holthuis <jan.holthuis@rub.de>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy
+// of the MPL was not distributed with this file, You can obtain one at
+// http://mozilla.org/MPL/2.0/.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+use clap::Parser;
+use ssh_key::{PrivateKey, Result};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(author, version, about)]
+#[command(propagate_version = true)]
+struct Cli {
+    /// File to parse.
+    #[arg(value_name = "FILE")]
+    path: PathBuf,
+}
+
+fn dump_private_key(key: PrivateKey) -> Result<()> {
+    println!("{:#?}", key);
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+    let private_key = PrivateKey::read_openssh_file(&args.path)?;
+    dump_private_key(private_key)
 }
